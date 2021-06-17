f2532c65a3009022ba45eafc21fe762506bd6df8
生活就像一坨 <63276095+2362400196@users.noreply.github.com>
2021-06-17 02:17:37 -0500

    模板2

--- kanglemoban.sh
+++ kanglemoban.sh
@@ -6,7 +6,7 @@ read -p "请选择：" mb
 if [ $mb == '1' ]; then
     echo "模板1"
     cd /vhs/kangle/nodewww/webftp/vhost/view/
-    wget -q https://raw.githubusercontent.com/2362400196/shell/main/default2.tar.gz
+    wget -q https://raw.githubusercontent.com/2362400196/shell/main/模板2.gz
     tar -zxvf 模板2.gz
     echo "完成，去kangle后台切换模板为default2，并清理模板缓存"
     chmod a+x kanglemoban.sh
