414fec89167fa543526fb9208587d2172ab44b3e
生活就像一坨 <63276095+2362400196@users.noreply.github.com>
2021-06-19 18:10:09 +0800

    kangle模板脚本

--- kanglemoban.sh
+++ kanglemoban.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 clear
-echo "1.简洁大气一号模板。演示https://cdn.jsdelivr.net/gh/2362400196/zhu/750b56bf6055a94826d0317390d49b0b.png"
+echo "1.简洁大气一号模板"
 echo "2.模板2"
 read -p "请选择：" mb
 if [ $mb == '1' ]; then
     echo "模板1"
-    cd /vhs/kangle/nodewww/webftp/vhost/view/
-    wget -q https://raw.githubusercontent.com/2362400196/shell/main/模板2.tar.gz
-    tar -zxvf 模板2.tar.gz
-    echo "完成，去kangle后台切换模板为default2，并清理模板缓存"
+    cd /vhs/kangle/nodewww/webftp/vhost/view//default
+    rm -rf /vhs/kangle/nodewww/webftp/vhost/view/default/*
+    wget -q https://github.com/2362400196/shell/raw/main/ep.zip
+    unzip ep.zip
+    echo "完成，去kangle后台清理模板缓存"
     chmod a+x kanglemoban.sh
     ./kanglemoban.sh
 elif [ $kl == '2' ]; then
