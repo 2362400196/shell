f8f3b07f84fda4defe7749e195064b4eb0044551
生活就像一坨 <63276095+2362400196@users.noreply.github.com>
2021-06-22 12:38:41 +0800

    Update bfkangle.sh

--- bfkangle.sh
+++ bfkangle.sh
@@ -35,7 +35,7 @@ cd /backup
 wget -q https://github.com/2362400196/shell/raw/main/kangle.py -O kangle.py
 chmod a+x kangle.py
 python3 /backup/kangle.py
-echo "python3 /backup/kangle.py" >bf.sh
+echo "python3 /backup/kangle.py" >/root/bf.sh
 crontab -r
 echo "* * * * * /bf.sh >>bf.log" >>/var/spool/cron/root
 echo "已经设置了每天24点准时备份"
