a6854f51ecd0d9a4b6eda298ed06f823f2396b71
生活就像一坨 <63276095+2362400196@users.noreply.github.com>
2022-01-27 08:38:34 +0800

    v2面板

--- v2.sh
+++ v2.sh
@@ -15,6 +15,10 @@ systemctl restart x-ui
 clear
 echo "安装完成！"
 echo -e "\033[31m 后台访问地址：你的ip+:54321 \033[0m"
+echo -e "\033[31m 后台访问地址：账号密码都是admin，如果进不去后台请开端口 \033[0m"
+echo -e "\033[31m 开端口方法：开端口：firewall-cmd --add-port=端口/tcp --permanent
+
+载入端口：firewall-cmd --reload \033[0m"
 echo "
 x-ui 管理脚本使用方法: 
 ----------------------------------------------
